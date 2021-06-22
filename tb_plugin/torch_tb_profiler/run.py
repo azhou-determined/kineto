5ef08ccaa03b10bb5afaae02b8db5cbdfbebc36d
Anda <anda@determined.ai>
2021-06-22 10:18:36 -0700

    logger

--- tb_plugin/torch_tb_profiler/run.py
+++ tb_plugin/torch_tb_profiler/run.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # --------------------------------------------------------------------------
 from . import consts
+import utils
 logger = utils.get_logger()
 
 
